fc467f474a45f9fe785a21ef15532bfb54495825
keremsan51 <196552340+keremsan51@users.noreply.github.com>
2025-03-07 15:46:42 +0100

    DexNav Testing

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -49,9 +49,9 @@
 #define FLAG_RANDOM_FIXED_MON        0x23 
 #define FLAG_RANDOM_STARTERS         0x24 
 #define DYNAMIC_LEVEL_WILD_POKEMON   0x25 
-#define FLAG_UNUSED_0x026    0x26 // Unused Flag
-#define FLAG_UNUSED_0x027    0x27 // Unused Flag
-#define FLAG_UNUSED_0x028    0x28 // Unused Flag
+#define FLAG_DN_SEARCHING            0x26 
+#define FLAG_DN_GET                  0x27 
+#define FLAG_DN_DETECTOR_MODE        0x28 
 #define FLAG_UNUSED_0x029    0x29 // Unused Flag
 #define FLAG_UNUSED_0x02A    0x2A // Unused Flag
 #define FLAG_UNUSED_0x02B    0x2B // Unused Flag
